82fa15f2653cd6d10fe13ea1590fb40bceae36bc
joshuajosephjefries <79646955+joshuajosephjefries@users.noreply.github.com>
2022-02-17 13:01:14 +0530

    Creating a file

--- local_file.tf
+++ local_file.tf
@@ -1,5 +1,5 @@
-resource "local_filecreation" "Introduction" {
-    local_file = "/home/ec2-user/new_file.txt"
+resource "local_file" "Introduction" {
+    filename = "/home/ec2-user/new_file.txt"
     content = "Hi, my name is Joshua Joseph Jefries. I am learning Terraform"
     file_permission = "0777"
     directory_permission = "0777"
