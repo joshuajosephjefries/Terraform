4f656e2da52daf696632182b47b48faef7e5301f
joshuajosephjefries <79646955+joshuajosephjefries@users.noreply.github.com>
2022-02-17 13:12:27 +0530

    Creating local file on AWS

--- local_file.tf
+++ local_file.tf
@@ -1,3 +1,6 @@
+/* This task creates a file called new_file.txt on the local directory of AWS
+   It also adds the content and sets the permission to the file and the folder it resides on */
+
 resource "local_file" "Introduction" {
     filename = "/home/ec2-user/new_file.txt"
     content = "Hi, my name is Joshua Joseph Jefries. I am learning Terraform"
