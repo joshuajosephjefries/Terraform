967a47171b083c79679fe14b1985570e0bc1d4af
joshuajosephjefries <bjosephjefries@gmail.com>
2022-02-25 01:43:14 +0530

    updated main.tf

--- Variables examples/main.tf
+++ Variables examples/main.tf
@@ -1,3 +1,8 @@
 resource "random_pet" "g7-countries-leader"{
-    prefix = var.countries[1] # Canada
+    prefix = var.countries_list[1] # Canada
+}
+
+resource "local_file" "myfile" {
+    filename = "/home/ec2-user/terraform"
+    content = var.countries_maps["statement2"] # The G7 is an informal grouping of seven of the world's advanced economies
 }
