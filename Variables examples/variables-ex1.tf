f671da3b80577a9ee009558851ef6f16665ff117
joshuajosephjefries <bjosephjefries@gmail.com>
2022-02-25 01:53:15 +0530

    variable as list

--- Variables examples/variables-ex1.tf
+++ Variables examples/variables-ex1.tf
@@ -1,6 +1,6 @@
 # Variables with type as list
 
-variable "countries" {
+variable "countries_list" {
     default = ["USA", "Canada", "Germany", "UK", "Italy", "France", "Japan"]
     type = list
     description = "List of countries"
