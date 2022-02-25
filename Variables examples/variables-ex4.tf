d54e2ec510c2ffc098f32e0a474bc67aee84e530
joshuajosephjefries <bjosephjefries@gmail.com>
2022-02-25 12:28:22 +0530

    variable as map of number

--- Variables examples/variables-ex4.tf
+++ Variables examples/variables-ex4.tf
@@ -1,14 +1,14 @@
-# Variables with type as maps of string
+# Variables with type as maps of number
 
 variable "countries_map_number" {
     default = {
-        canada = "1"
-        USA = "3"
-        Germany = "5"
-        UK = "4"
-        Italy = "7"
-        France = "2"
-        Japan = "6"
+        "canada" = 1
+        "USA" = 3
+        "Germany" = 5
+        "UK" = 4
+        "Italy" = 7
+        "France" = 2
+        "Japan" = 6
     }
     type = map(number)
     description = "Serial numbers of countries"
