cf76e9eff249a9b0c36f1306e3414137efc04917
joshuajosephjefries <bjosephjefries@gmail.com>
2022-02-25 18:57:57 +0530

    Variable that prompts for user input

---
+++ Variables examples/variables-ex7.tf
@@ -0,0 +1,10 @@
+# Variables that prompt for user input
+
+variable "count_no" {
+    # This block is left empty without variables for user input prompt. Expecting Type -> number. 
+    # Since we did not mention any type, it defaultly considers type as "all".
+}
+variable "country" {
+    # This block is left empty without variables for user input prompt. Expecting Type -> string
+    # Since we did not mention any type, it defaultly considers type as "all".
+}
