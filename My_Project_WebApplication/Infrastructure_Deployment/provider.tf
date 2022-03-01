10a9d3b123782d8f55af9f5aeff70e617591123e
joshuajosephjefries <79646955+joshuajosephjefries@users.noreply.github.com>
2022-03-01 20:20:50 +0530

    Updated Provider.tf

--- My_Project_WebApplication/Infrastructure_Deployment/provider.tf
+++ My_Project_WebApplication/Infrastructure_Deployment/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAUVU2B7VC6JC7UVWF"
-    secret_key = "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"
+    access_key = "Get_Access_key_from_IAM"
+    secret_key = "Get_SECRET_key_from_IAM"
 }
