04cdb5b15e09433c3e0af87dd429514cc64ebeed
joshuajosephjefries <bjosephjefries@gmail.com>
2022-03-07 02:02:44 +0530

    Updated the private key generation

--- My_Project_WebApplication/Infrastructure_Deployment/output.tf
+++ My_Project_WebApplication/Infrastructure_Deployment/output.tf
@@ -10,10 +10,23 @@ resource "local_file" "my_public_ip" {
 }
 
 output "public_key" {
-    value = tls_private_key.example.public_key_pem
+    value = tls_private_key.example.public_key_openssh
 }
 
 resource "local_file" "public_key" {
-    content  = tls_private_key.example.public_key_pem
+    content  = tls_private_key.example.public_key_openssh
     filename = "/home/ec2-user/My_Project_WebApplication/Ansible_Application_Deployment/public_key.pem"
 }
+
+output "private_key" {
+    value = tls_private_key.example
+    sensitive = true
+}
+
+# Change path for your specific OS
+resource "local_file" "private_key" {
+    content  = tls_private_key.example.private_key_pem
+    filename = "C:\\Users\\josep\\Desktop\\private_key.pem"
+}
+
+
