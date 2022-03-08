207d3296c760835ae66a7a298108083c6951c005
joshuajosephjefries <bjosephjefries@gmail.com>
2022-03-08 20:00:59 +0530

    Changed the path

--- My_Project_WebApplication/Infrastructure_Deployment/output.tf
+++ My_Project_WebApplication/Infrastructure_Deployment/output.tf
@@ -1,12 +1,10 @@
-# This file consists of the output value that is to be used by Ansible script. 
-
 output "vm_public_ip" {
     value = aws_eip.my_elastic_ip.public_ip
 }
 
 resource "local_file" "my_public_ip" {
     content  = aws_eip.my_elastic_ip.public_ip
-    filename = "/home/ec2-user/Terraform/My_Project_WebApplication/Ansible_Application_Deployment/my_ip.txt"
+    filename = "C:\\Users\\josep\\Desktop\\Terraform\\my_ip.txt"
 }
 
 output "public_key" {
@@ -15,17 +13,13 @@ output "public_key" {
 
 resource "local_file" "public_key" {
     content  = tls_private_key.example.public_key_openssh
-    filename = "/home/ec2-user/Terraform/My_Project_WebApplication/Ansible_Application_Deployment/public_key.pem"
+    filename = "C:\\Users\\josep\\Desktop\\Terraform\\public_key.pem"
 }
 
 output "private_key" {
     value = tls_private_key.example
     sensitive = true
 }
-
-# Change path for your specific OS. 
-# This would save the private key in the path specified
-# Convert this private key from pem to ppk using puttygen so as to access AppEC2Instance using Putty
 
 resource "local_file" "private_key" {
     content  = tls_private_key.example.private_key_pem
