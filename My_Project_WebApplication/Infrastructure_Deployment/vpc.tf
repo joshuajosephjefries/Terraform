414ba8b9b2a64f439ca1a3c0ae164f626ec2c296
joshuajosephjefries <bjosephjefries@gmail.com>
2022-03-13 00:22:51 +0530

    Changed CIDR ans subnets

--- My_Project_WebApplication/Infrastructure_Deployment/vpc.tf
+++ My_Project_WebApplication/Infrastructure_Deployment/vpc.tf
@@ -4,10 +4,10 @@ This sets public subnets, availability zone, & CIDR.
 module "vpc" {
     source = "terraform-aws-modules/vpc/aws"
     name = "vpc_for_project"
-    cidr = "172.48.0.0/16"
+    cidr = "192.168.0.0/16"
 
     azs = ["us-east-1a"]
-    public_subnets = ["172.48.0.0/20", "172.48.16.0/20", "172.48.32.0/20", "172.48.48.0/20"]
+    public_subnets = ["192.168.0.0/20", "192.168.16.0/20", "192.168.32.0/20"]
     tags = {
         Name = "Project_VPC"
         createdBy = "JoshuaJosephJefries"
