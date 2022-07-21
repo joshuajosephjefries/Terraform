203ac2d411de3fa49e1fcce53f473dad17440f01
joshuajosephjefries <79646955+joshuajosephjefries@users.noreply.github.com>
2022-07-21 14:36:24 -0300

    Added bucket public access block (Sonarcloud.io)

--- aws_s3_bucket.tf
+++ aws_s3_bucket.tf
@@ -6,6 +6,15 @@ resource "aws_s3_bucket" "jjefries" {
     }
 }
 
+resource "aws_s3_bucket_public_access_block" "public-access-block" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "local_file" "myprofile" {
     filename         = "/home/ec2-user/terraform/profile1.txt"
     file_permission  = "0777"
